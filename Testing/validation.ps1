e21355f5d55e648c2a2ac6b24a4900005b4fe550
Jonas Schön <js@gefyra.de>
2026-07-06 09:23:32 +0200

    initial commit

--- Testing/validation.ps1
+++
@@ -1,6 +0,0 @@
-if (-not (Test-Path .\validator_cli.jar)){ 
-	(New-Object Net.WebClient).DownloadFile('https://github.com/hapifhir/org.hl7.fhir.core/releases/latest/download/validator_cli.jar', 'validator_cli.jar')
-	Write-Host "Downloaded validator_cli.jar"
-}
-
-java -jar validator_cli.jar -ig de.gematik.sterbefall#1.0.0 -language de -server 8081
--- Testing/validation.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -f "./validator_cli.jar" ]; then
-    echo "validator_cli.jar wird heruntergeladen..."
-    curl -L -o validator_cli.jar "https://github.com/hapifhir/org.hl7.fhir.core/releases/latest/download/validator_cli.jar"
-    echo "Download abgeschlossen."
-fi
-
-java -jar validator_cli.jar -ig "de.gematik.sterbefall#1.0.0" -language "de" -server 8081
